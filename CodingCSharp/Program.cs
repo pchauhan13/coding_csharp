8645c911af376d3c7f568485f69c9bbde53e1b2b
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-27 18:48:07 +0530

    taking input from the user and displaying variables on the console

--- CodingCSharp/Program.cs
+++ CodingCSharp/Program.cs
@@ -8,6 +8,16 @@ namespace CodingCSharp
         {
             Program program = new Program();
             program.Print();
+
+            var x = Console.ReadLine();
+            Console.WriteLine(x);
+            Console.WriteLine($"Hello {x}");
+
+            int y = 6;
+            Console.WriteLine(y);
+
+            String str = "Prashant";
+            Console.WriteLine(str + " is weird");
         }
 
         void Print()
