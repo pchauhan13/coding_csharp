4aef3fad3bd3b3988c12dd3a3716d3b04643c2ef
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-08 15:57:44 +0530

    creating a class with default constructor and instantiating it

---
+++ CodingCSharp/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingCSharp
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book()
+        {
+            Console.WriteLine("Creating a new book object...");
+        }
+    }
+}
